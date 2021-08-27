c70d6ca48a9b1c8f68330f27895b830adebb87e1
amatla <amatlaow@gmail.com>
2021-08-27 09:54:14 +0200

    Implements color * color multiplication and color * scalar multiplication

--- src/color.js
+++ src/color.js
@@ -1,13 +1,29 @@
 const constants = require('./constants');
 
 const { dPoints } = constants;
+
+/**
+ *
+ * @class Color
+ */
 class Color {
+  /**
+   *
+   * @param {Number} red
+   * @param {Number} green
+   * @param {Number} blue
+   */
   constructor(red = 0.0, green = 0.0, blue = 0.0) {
     this.red = red;
     this.green = green;
     this.blue = blue;
   }
 
+  /**
+   *
+   * @param  {...Color} colors
+   * @returns {Color}
+   */
   static add(...colors) {
     const sum = colors.reduce((acc, curr) => {
       acc.red += curr.red;
@@ -22,6 +38,11 @@ class Color {
     );
   }
 
+  /**
+   *
+   * @param  {...Color} colors
+   * @returns {Color}
+   */
   static subtract(...colors) {
     const diff = colors.reduce((acc, curr) => {
       acc.red -= curr.red;
@@ -33,6 +54,33 @@ class Color {
       Number(diff.red.toFixed(dPoints)),
       Number(diff.green.toFixed(dPoints)),
       Number(diff.blue.toFixed(dPoints)),
+    );
+  }
+
+  /**
+   *
+   * @param  {...Color} colors
+   * @returns {Color}
+   */
+  static multiply(...colors) {
+    const mult = colors.reduce((acc, curr) => {
+      acc.red *= curr.red;
+      acc.green *= curr.green;
+      acc.blue *= curr.blue;
+      return acc;
+    });
+    return new Color(
+      Number(mult.red.toFixed(dPoints)),
+      Number(mult.green.toFixed(dPoints)),
+      Number(mult.blue.toFixed(dPoints)),
+    );
+  }
+
+  multiply(num) {
+    return new Color(
+      Number((this.red * num).toFixed(dPoints)),
+      Number((this.green * num).toFixed(dPoints)),
+      Number((this.blue * num).toFixed(dPoints)),
     );
   }
 }
--- test/color.test.js
+++ test/color.test.js
@@ -19,4 +19,14 @@ describe('Color:', () => {
     const c2 = new Color(0.7, 0.1, 0.25);
     expect(Color.subtract(c1, c2)).toEqual(new Color(0.2, 0.5, 0.5));
   });
+  test('Multiplying colors', () => {
+    const c1 = new Color(1, 0.2, 0.4);
+    const c2 = new Color(0.9, 1, 0.1);
+    expect(Color.multiply(c1, c2)).toEqual(new Color(0.9, 0.2, 0.04));
+  });
+  test('Mulyplying a color by a scalar:', () => {
+    const c = new Color(0.2, 0.3, 0.4);
+    const num = 2.2;
+    expect(c.multiply(num)).toEqual(new Color(0.44, 0.66, 0.88));
+  });
 });
