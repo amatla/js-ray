c9643664653091cc86e940bedca466c169fd21f4
amatla <amatlaow@gmail.com>
2021-08-27 11:54:21 +0200

    Implements Canvas Class

--- src/canvas.js
+++ src/canvas.js
@@ -1,6 +1,15 @@
 const Color = require('./color');
 
+/**
+ *
+ * @class Canvas
+ */
 class Canvas {
+  /**
+   *
+   * @param {Number} width
+   * @param {Number} length
+   */
   constructor(width = 10, length = 10) {
     this.width = width;
     this.heigth = length;
@@ -8,15 +17,53 @@ class Canvas {
     this.fill(Color.BLACK);
   }
 
+  /**
+   *
+   * @param {Color} color
+   * @returns {Canvas}
+   */
   fill(color) {
     for (let y = 0; y < this.heigth; y += 1) {
-      for (let x = 0; x < this.width; x += 1) {
-        this.pixels[(x, y)] = color;
-      }
+      this.pixels[y] = new Array(this.width).fill(color);
     }
+    return this;
+  }
+
+  /**
+   *
+   * @param {Number} x
+   * @param {Number} y
+   * @returns {Boolean}
+   */
+  hasPixel(x, y) {
+    return x >= 0 && x < this.width && y >= 0 && y < this.heigth;
+  }
+
+  /**
+   *
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Color} color
+   * @returns {Boolean}
+   */
+  writePixel(x, y, color) {
+    if (this.hasPixel(x, y) && color instanceof Color) {
+      this.pixels[x][y] = color;
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   *
+   * @param {Number} x
+   * @param {Number} y
+   * @returns {(Color) || (false)}
+   */
+  pixelAt(x, y) {
+    if (this.hasPixel(x, y)) return this.pixels[x][y];
+    return false;
   }
 }
 
-const c = new Canvas(10, 10);
-console.log(c);
 module.exports = Canvas;
--- test/canvas.test.js
+++ test/canvas.test.js
@@ -6,10 +6,16 @@ describe('Canvas:', () => {
     const c = new Canvas(10, 20);
     expect(c.width).toBe(10);
     expect(c.heigth).toBe(20);
-    for (let y = 0; y < c.heigth; y += 1) {
-      for (let x = 0; x < c.width; x += 1) {
-        expect(c.pixels[(x, y)]).toEqual(Color.BLACK);
+    for (let x = 0; x < c.heigth; x += 1) {
+      for (let y = 0; y < c.width; y += 1) {
+        expect(c.pixels[x][y]).toEqual(Color.BLACK);
       }
     }
+  });
+  test('Writing a pixel', () => {
+    const c = new Canvas(10, 20);
+    const red = new Color(1, 0, 0);
+    c.writePixel(2, 3, red);
+    expect(c.pixelAt(2, 3)).toEqual(new Color(1, 0, 0));
   });
 });
